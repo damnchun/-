4cf27100be197c4934e0842cf0a6f5a269966df3
damnchun <3553814537@qq.com>
2024-11-08 22:21:19 +0800

    Add files via upload

--- lun_kuo_xi_shu.py
+++ lun_kuo_xi_shu.py
@@ -5,12 +5,14 @@ import numpy as np
 from sklearn.cluster import SpectralClustering
 from sklearn.cluster import DBSCAN
 #定义轮廓系数函数，方便其他文件调用
-def chioce_best (features_numpy,min_number=2,max_number=10,type = "mini",min_esp = 0.1,max_esp =0.2 ):
+def chioce_best (features_numpy,min_number=2,max_number=10,
+                 type = "mini",min_esp = 0.1,max_esp =0.2 ):
     values = []
     #minibach聚类
     if  type == "mini":
         for cluster_number in range(min_number,max_number):
-            minibach_program = MiniBatchKMeans(n_clusters=cluster_number,batch_size=5000)
+            minibach_program = MiniBatchKMeans(n_clusters=cluster_number,
+                                               batch_size=5000)
             cluster_result = minibach_program.fit_predict(features_numpy)
             value = silhouette_score(X=features_numpy,labels=cluster_result)
             values.append(value)
@@ -26,7 +28,8 @@ def chioce_best (features_numpy,min_number=2,max_number=10,type = "mini",min_esp
             spectral_program = SpectralClustering(n_clusters=cluster_number)
             spectral_program.fit(features_numpy)
             clustering_result = spectral_program.labels_
-            value = silhouette_score(X=features_numpy, labels=clustering_result_result)
+            value = silhouette_score(X=features_numpy,
+                                     labels=clustering_result_result)
             values.append(value)
         #可视化输出
         plt.plot(np.arange(min_number,max_number),values,c="b")
@@ -37,14 +40,15 @@ def chioce_best (features_numpy,min_number=2,max_number=10,type = "mini",min_esp
     #DBSCAN
     if type == "db":
         for sample_numbers  in range(min_number,max_number):
-            for epsnumber in range (10*min_esp,10*max_esp):#循环双参数
+            for epsnumber in range (int(10*min_esp),int(10*max_esp)):#循环双参数
                 epsn = epsnumber/10
                 density_program = DBSCAN(eps=epsn, min_samples=sample_numbers)
                 density_program.fit(features_numpy)
                 clustering_result = density_program.labels_
                 # 检查是否至少有两个聚类
                 if len(np.unique(clustering_result)) < 2:
-                    print("Error: Less than two clusters after filtering noise. Cannot compute silhouette score.")
+                    print("Error: Less than two clusters after "
+                          "filtering noise. Cannot compute silhouette score.")
                     value = np.nan
                 else:
                     # 计算轮廓系数
